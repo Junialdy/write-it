ad5ae98c09225d279564c950f410b0453a120b2a
junialdy <junialdyp0629@gmail.com>
2025-05-11 23:14:39 +0700

    style: update siteConfig

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,13 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
+        <span className={title()}>Inside&nbsp;</span>
+        <span className={title({ color: "violet" })}>Design</span>
         <span className={title()}>
-          websites regardless of your design experience.
+          : Stories and interviews.
         </span>
         <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+          Subscribe to learn about new product features, the latest in technology, ans updates.
         </div>
       </div>
 
--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Write it",
+  description: "Make beautiful blog regardless of your design experience.",
   navItems: [
     {
       label: "Home",
