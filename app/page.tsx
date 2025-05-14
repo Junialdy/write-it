673cd1aea947120b05ca4ef4b72d6226d10f9fc3
junialdy <junialdyp0629@gmail.com>
2025-05-14 23:30:34 +0700

    style: testing primitives components

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,10 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { title } from "@/components/primitives";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Inside&nbsp;</span>
-        <span className={title({ color: "violet" })}>Design</span>
-        <span className={title()}>
-          : Stories and interviews.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Subscribe to learn about new product features, the latest in technology, ans updates.
-        </div>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
+      <div className="inline-block max-w-lg text-center justify-center">
+        <h1 className={title()}>Inside design: Stories and interviews</h1>
       </div>
     </section>
   );
